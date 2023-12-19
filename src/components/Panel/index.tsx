0b4276a45f04f58e7b978a1af8f2ddbb00baf792
ChandraRaditya <chandra@refactory.id>
2023-12-19 15:48:29 +0700

    feat: add spending card

--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import BaseCard from "../BaseCard";
 import FinancialCard from "../FinancialCard";
+import SpendingCard from "../SpendingCard";
 
 const Panel = () => {
+  const spendingData = [
+    {
+      image: "illustrations/shopping",
+      detail: "Online Store",
+      date: new Date(),
+    },
+    {
+      image: "illustrations/hospital",
+      detail: "Pay the hospital",
+      date: new Date(),
+    },
+    {
+      image: "illustrations/ticket",
+      detail: "Ticket",
+      date: new Date(),
+    },
+  ];
   return (
     <div className="p-8 flex flex-col gap-6">
       <FinancialCard
@@ -11,6 +29,7 @@ const Panel = () => {
         incomePercentage={40}
         incomeTotal={2656}
       />
+      <SpendingCard spendingData={spendingData} />
     </div>
   );
 };
